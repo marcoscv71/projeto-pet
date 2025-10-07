1d5d55db510af2e102ba2a32f251cd37c85e8c25
Marcos Vinícius Silva <marcoscv71@gmail.com>
2025-10-07 16:17:58 -0300

    feat: implementing people_repository

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,42 @@
+from sqlalchemy.orm.exc import NoResultFound
+
+from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.entities.pets import PetsTable
+
+
+class PeopleRepository:
+    def __init__(self, db_connection: str) -> None:
+        self.__db_connection = db_connection
+
+    def insert_person(
+        self, first_name: str, last_name: str, age: int, pet_id: int
+    ) -> None:
+        with self.__db_connection as conn:
+            try:
+                person_data = PeopleTable(
+                    first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
+                )
+                conn.session.add(person_data)
+                conn.session.commit()
+            except Exception as e:
+                conn.session.rollback()
+                raise e
+
+    def get_person(self, person_id: int) -> PeopleTable | None:
+        with self.__db_connection as conn:
+            try:
+                person = (
+                    conn.session.query(PeopleTable)
+                    .outerjoin(PetsTable, PetsTable.id == PeopleTable.pet_id)
+                    .filter(PeopleTable.id == person_id)
+                    .with_entities(
+                        PeopleTable.first_name,
+                        PeopleTable.last_name,
+                        PetsTable.name.label("pet_name"),
+                        PetsTable.type.label("pet_type"),
+                    )
+                    .one()
+                )
+                return person
+            except NoResultFound:
+                return None
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -2,9 +2,10 @@ import pytest
 
 from src.models.sqlite.settings.connection import db_connection_handler
 
+from .people_repository import PeopleRepository
 from .pets_repository import PetsRepository
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="Not a real test, just for demonstration")
@@ -20,3 +21,22 @@ def test_delete_pet():
 
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+
+@pytest.mark.skip(reason="Not a real test, just for demonstration")
+def test_insert_person():
+    first_name = "John"
+    last_name = "Doe"
+    age = 30
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="Not a real test, just for demonstration")
+def test_get_person():
+    repo = PeopleRepository(db_connection_handler)
+    person = repo.get_person(1)
+    print()
+    print(person)
