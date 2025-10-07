ce56abb5d2157412bef6ac90fea1856708ff0182
Marcos Vinícius Silva <marcoscv71@gmail.com>
2025-10-07 14:33:54 -0300

    feat: implementing pets_repository with first test

---
+++ src/models/sqlite/repositories/pets_repository.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm.exc import NoResultFound
+
+from src.models.sqlite.entities.pets import PetsTable
+
+
+class PetsRepository:
+    def __init__(self, db_connection) -> None:
+        self.__db_connection = db_connection
+
+    def list_pets(self) -> list[PetsTable]:
+        with self.__db_connection as conn:
+            try:
+                pets = conn.session.query(PetsTable).all()
+                return pets
+            except NoResultFound:
+                return []
+
+    def delete_pets(self, name: str) -> None:
+        with self.__db_connection as conn:
+            try:
+                conn.session.query(PetsTable).filter(PetsTable.name == name).delete()
+                conn.session.commit()
+            except Exception as e:
+                conn.session.rollback()
+                raise e
---
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -0,0 +1,39 @@
+from unittest import mock
+
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+
+from src.models.sqlite.entities.pets import PetsTable
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+
+
+class MockConnection:
+    def __init__(self):
+        self.session = UnifiedAlchemyMagicMock(
+            data=[
+                (
+                    [mock.call.query(PetsTable)],  # query
+                    [
+                        PetsTable(name="Fido", type="dog"),
+                        PetsTable(name="Whiskers", type="cat"),
+                    ],  # resultado
+                )
+            ]
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+def test_list_pets():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+    response = repo.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_called_once()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response[0].name == "Fido"
